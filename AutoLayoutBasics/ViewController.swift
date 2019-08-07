bfd01c1eddcc59a036e80eaf9eb1f37f40540d83
Michael Reinders <Michael.H.Reinders@Gmail.com>
2019-08-06 17:19:24 -0700

    Initial Commit

---
+++ AutoLayoutBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutBasics
+//
+//  Created by Michael Reinders on 8/6/19.
+//  Copyright © 2019 Michael Reinders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
